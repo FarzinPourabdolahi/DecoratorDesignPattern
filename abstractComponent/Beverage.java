efef6a5b5c34b089d8951c5cb2c79b06557fb656
FarzinPourabdolahi <Pourabdolahi.Farzin@gmail.com>
2024-01-15 19:56:50 +0330

    Beverage more functionality

--- abstractComponent/Beverage.java
+++ abstractComponent/Beverage.java
@@ -1,7 +1,9 @@
 package abstractComponent;
 
 public abstract class Beverage {
-    private String description = "Unknown beverage!";
+    public enum Size { TALL, GRANDE, VENTI };
+    Size size = Size.TALL;
+    private String description = "Unknown Beverage";
 
     public String getDescription(){
         return  description;
@@ -9,6 +11,13 @@ public abstract class Beverage {
 
     public void setDescription(String description){
         this.description = description;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+    public Size getSize() {
+        return this.size;
     }
     public abstract float cost();
 
